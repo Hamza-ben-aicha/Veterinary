8a3412ad552318f8f41e90883113c8ecbc5bf996
Hamza-ben-aicha <75418750+Hamza-ben-aicha@users.noreply.github.com>
2022-01-19 23:32:59 +0100

    like & delete & modifie per account

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -35,7 +35,7 @@ const Post =({post, setCurrentId})=>{
             {moment(post.createdAt).fromNow()}
           </Typography>
         </div>
-        {(user?.result?.googleId === post?.creator || user.result._id === post.creator)&&(
+        {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator)&&(
             <div className={classes.overlay2}>
               <Button style={{ color: "white" }} size="small" onClick={() => setCurrentId(post._id)}>
                 <MoreHorizIcon />
@@ -52,7 +52,7 @@ const Post =({post, setCurrentId})=>{
           <Button size="small" color="primary" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))}>
             <Likes/>
           </Button>
-          {(user?.result?.googleId === post?.creator || user.result._id === post.creator)&&(
+          {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator)&&(
               <Button size="small" color="secondary" onClick={() => dispatch(deletePost(post._id))}>
                 <DeleteIcon fontSize="small" /> Delete 
               </Button>
